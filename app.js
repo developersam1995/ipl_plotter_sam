b09ed65ab2c9f50b34ebd8b0f8579d8bed4e4a54
sam1995 <samrudh.kumar@mountblue.io>
2018-05-31 15:05:44 +0530

    alpha

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ const app = express();
 app.use(express.static('./public'));
 
 app.get('/plotter', (req, res) => {
-    res.sendfile('./index.html');
+  res.sendfile('./index.html');
 });
 
 app.get('/plotter/:id', (req, res) => {
-    let dataId = req.params.id;
-    if(dataId == 1) {
-        fs.readFile('./appData/matchPerSeason.json', 'utf8', (err, data) => {
-            res.json(data);
-        });
-    }
-    else if(dataId == 2) {
-        fs.readFile('./appData/wonPerSeason.json', 'utf8', (err, data) => {
-            res.json(data);
-        });
-    }
-    else if(dataId == 3) {
-        res.json([1,2,3]);
-    }
-    else if(dataId == 4) {
-        res.json([1,2,3]);
-    }
-    else if(dataId == 5) {
-        res.json([1,2,3]);
-    }
-    else res.sendStatus(404);
+  let dataId = req.params.id;
+  if (dataId == 1) {
+    fs.readFile('./appData/matchPerSeason.json', 'utf8', (err, data) => {
+      res.json(data);
+    });
+  }
+  else if (dataId == 2) {
+    fs.readFile('./appData/wonPerSeason.json', 'utf8', (err, data) => {
+      res.json(data);
+    });
+  }
+  else if (dataId == 3) {
+    res.json([1, 2, 3]);
+  }
+  else if (dataId == 4) {
+    res.json([1, 2, 3]);
+  }
+  else if (dataId == 5) {
+    res.json([1, 2, 3]);
+  }
+  else res.sendStatus(404);
 });
 
 app.listen(3000);
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,22 +1,104 @@
-$(document).ready(function(){
-  $('.nav').on('click', ajaxCallAndDisplay);
+$(document).ready(function () {
+  $('.nav').on('click', ajaxCall);
   $('#close-btn-modal').on('click', hideModal);
 
-  function ajaxCallAndDisplay(event) {
-    //   $('.modal').css('display', 'block');
-      let targetId = event.target.id;
-      $.ajax({
-        type: 'GET',
-        dataType: "json",
-        url: `/plotter/${targetId}`,
-        success: (data) => {
-           console.log(data);
-        }
-      });
+  function ajaxCall(event) {
+    let targetId = event.target.id;
+    $.ajax({
+      type: 'GET',
+      dataType: "json",
+      url: `/plotter/${targetId}`,
+      success: (data) => {
+        displayModal(data, targetId);
+      }
+    });
   }
 
   function hideModal() {
-      $('.modal').css('display', 'none');
+    $('.modal').css('display', 'none');
+  }
+
+  function displayModal(data, id) {
+    $('.modal').css('display', 'block');
+    let chartInfo = JSON.parse(data);
+    let seasonList = Object.keys(chartInfo).map((ele) => {
+      return parseInt(ele);
+    });
+    let startSeason = seasonList.reduce((ele1, ele2) => {
+      return Math.min(ele1,ele2);
+    });
+    let endSeason = seasonList.reduce((ele1, ele2) => {
+      return Math.max(ele1,ele2);
+    });
+    let numberPlayed = [];
+    for(let i = startSeason; i <= endSeason; i++) {
+      numberPlayed.push(chartInfo[i]);
+    }
+    if (id == 1) {
+      Highcharts.chart('modal-content', {
+
+        title: {
+          text: 'Number Of Matches Played Per Season'
+        },
+
+        subtitle: {
+          text: '2008-2017'
+        },
+
+        yAxis: {
+          title: {
+            text: 'Number of Matches Played'
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle'
+        },
+
+        plotOptions: {
+          series: {
+            label: {
+              connectorAllowed: false
+            },
+            pointStart: startSeason
+          }
+        },
+
+        series: [{
+          name: 'Number of matches played',
+          data: numberPlayed
+        }],
+
+        responsive: {
+          rules: [{
+            condition: {
+              maxWidth: 500
+            },
+            chartOptions: {
+              legend: {
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom'
+              }
+            }
+          }]
+        }
+
+      });
+    }
+    else if (id == 2) {
+
+    }
+    else if (id == 3) {
+
+    }
+    else if (id == 4) {
+
+    }
+    else if (id == 5) {
+
+    }
   }
 
 });
